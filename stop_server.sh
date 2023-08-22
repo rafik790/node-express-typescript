cbbd96c6a1ca3b65051f9fd993819f20be5a7423
Rafik Mondal <70977282+rafik790@users.noreply.github.com>
2023-08-23 01:04:42 +0530

    change stop script

--- stop_server.sh
+++ stop_server.sh
@@ -2,7 +2,7 @@
 # Stop all servers and start the server
 #sudo pm2 stop all
 # checking if 'appname' running in pm2
-PM2_EXIST=$(if pm2 list 2> /dev/null | grep -q index; then echo "Yes" ; else echo "No" ; fi)
+PM2_EXIST=$(if sudo pm2 list 2> /dev/null | grep -q index; then echo "Yes" ; else echo "No" ; fi)
 if [ $PM2_EXIST = Yes ] ; then
     sudo pm2 stop all
     echo "Stop index."
