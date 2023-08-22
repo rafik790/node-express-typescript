4e693c1826b978cb564ec928b0e189fb3fb73cdc
Rafik Mondal <70977282+rafik790@users.noreply.github.com>
2023-08-23 00:52:39 +0530

    redeploy

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT||3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('Welcome to Express + TypeScript Server');
   });
   
 app.listen(port, () => {
