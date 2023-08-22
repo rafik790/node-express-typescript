86524e41f827b3c100d38af5c2c2d065767e6a73
Rafik Mondal <70977282+rafik790@users.noreply.github.com>
2023-08-23 00:45:01 +0530

    added hook

--- stop_server.sh
+++ stop_server.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Stop all servers and start the server
-sudo pm2 stop all
+#sudo pm2 stop all
+# checking if 'appname' running in pm2
+PM2_EXIST=$(if pm2 list 2> /dev/null | grep -q index; then echo "Yes" ; else echo "No" ; fi)
+if [ $PM2_EXIST = Yes ] ; then
+    sudo pm2 stop all
+    echo "Stop index."
+fi
