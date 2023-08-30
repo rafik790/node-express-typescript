d6113abf6b64542f776c8408d401993e7ea72e3c
Rafik Mondal <70977282+rafik790@users.noreply.github.com>
2023-08-30 13:14:36 +0530

    change in message

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT||3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Express + TypeScript Server');
+    res.send('Welcome to Express + TypeScript Server + Deployed with CDK');
   });
   
 app.listen(port, () => {
